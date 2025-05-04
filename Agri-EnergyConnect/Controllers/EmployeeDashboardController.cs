7a5399db075a7887404d3346d5f5b43c1f981764
ST10260322 <st10260322@vcconnect.edu.za>
2025-05-04 23:33:01 +0200

    I created the COntrollers for Farmers and Employees

---
+++ Agri-EnergyConnect/Controllers/EmployeeDashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agri_EnergyConnect.Controllers
+{
+    public class EmployeeDashboardController : Controller
+    {
+        [Authorize(Roles = "Employee")] //This makes it specific to employees only
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Agri-EnergyConnect/Controllers/FarmerDashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agri_EnergyConnect.Controllers
+{
+    public class FarmerDashboardController : Controller
+    {
+        [Authorize(Roles = "Farmer")] //This line of code makes it that this is specific to farmers only
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
