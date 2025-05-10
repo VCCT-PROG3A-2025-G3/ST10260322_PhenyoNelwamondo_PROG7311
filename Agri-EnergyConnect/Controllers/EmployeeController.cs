47567cce2e51fa26c0c76961469380e23cdbf75c
ST10260322 <st10260322@vcconnect.edu.za>
2025-05-11 00:11:06 +0200

    An Employee can now add a farmer into the system

--- Agri-EnergyConnect/Controllers/EmployeeController.cs
+++ Agri-EnergyConnect/Controllers/EmployeeController.cs
@@ -1,16 +1,74 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Agri_EnergyConnect.Models;
 
 namespace Agri_EnergyConnect.Controllers
 {
+    [Authorize(Roles = "Employee")]
     public class EmployeeController : Controller
     {
-        [Authorize(Roles = "Employee")] //This makes it specific to employees only
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EmployeeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        
+        [HttpGet]
+        public IActionResult AddFarmer()
+        {
+            // We'll reuse the RegisterViewModel but set the Role to "Farmer" by default
+            var model = new RegisterViewModel
+            {
+                Role = "Farmer" // Default role for this form
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFarmer(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Force the role to be Farmer regardless of what was submitted
+                model.Role = "Farmer";
+
+                var existingUser = await _userManager.FindByEmailAsync(model.Email);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError(string.Empty, "Email is already registered.");
+                    return View(model);
+                }
+
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Surname = model.Surname
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.Role);
+                    return RedirectToAction("Index", "Employee");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
